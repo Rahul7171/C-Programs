05e0792867e869ba8a5fd8bd5aaaff452f96df56
Rahul7171 <69434755+Rahul7171@users.noreply.github.com>
2020-11-26 20:20:12 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Rahul Bagal on 26/11/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
