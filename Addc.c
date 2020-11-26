a23683f13b44e4dfcb7e50390ca6bf917b40aaec
Rahul7171 <69434755+Rahul7171@users.noreply.github.com>
2020-11-26 21:04:12 +0530

    Add files via upload

---
+++ Addc.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Add.C
+//
+//  Created by Rahul Bagal on 26/11/20.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    // insert code here...
+    int a,b;
+    printf("Enter the value of a:\n");
+    scanf("%d",&a);
+    printf("Enter the value of b:\n");
+    scanf("%d",&b);
+    printf("The sum is %d\n", a+b);
+    
+    return 0;
+}
